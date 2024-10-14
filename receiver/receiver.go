d7e20b8274fbe3f4e03f96778575d42fe5b5212a
BJS-kr <hahajeng1234@gmail.com>
2024-10-14 20:14:25 +0900

    refactor: delete receiver package

--- receiver/receiver.go
+++
@@ -1,9 +0,0 @@
-package receiver
-
-import "animalized/message"
-
-func Receive(inputProduceChannel <-chan *message.Input) {
-	for input := range inputProduceChannel {
-		Inputs.Enqueue(input)
-	}
-}
--- receiver/variables.go
+++
@@ -1,8 +0,0 @@
-package receiver
-
-import (
-	"animalized/message"
-	"animalized/queue"
-)
-
-var Inputs = queue.New[*message.Input]()
