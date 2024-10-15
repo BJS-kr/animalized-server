636b0b68f107f80af64c7485761337882d847591
BJS-kr <hahajeng1234@gmail.com>
2024-10-15 21:43:57 +0900

    feat: add validation

---
+++ producer/validate.go
@@ -0,0 +1,23 @@
+package producer
+
+import (
+	"animalized/message"
+	"animalized/packet"
+	"errors"
+)
+
+func validateInput(input *message.Input, userId string) error {
+	if input.UserId != userId {
+		return errors.New("user id not matched")
+	}
+
+	if input.Type == packet.INIT {
+		return errors.New("init packet not allowed in producer")
+	}
+
+	if input.Type == packet.MOVE && input.Direction == nil {
+		return errors.New("move packet did not include direction")
+	}
+
+	return nil
+}
