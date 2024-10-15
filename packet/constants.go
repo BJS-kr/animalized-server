51ebe9d756992f3357c5480d81778028d1e8ea46
BJS-kr <hahajeng1234@gmail.com>
2024-10-15 21:43:31 +0900

    fix: change constant position for correct iota calc

--- packet/constants.go
+++ packet/constants.go
@@ -1,9 +1,9 @@
 package packet
 
 const (
-	BUFFER_SIZE            = 4096
-	INPUT_PACKET_DELIMITER = '$'
-	INIT                   = iota + 1
+	INIT = iota + 1
 	MOVE
 	ATTACK
+	BUFFER_SIZE            = 4096
+	INPUT_PACKET_DELIMITER = '$'
 )
