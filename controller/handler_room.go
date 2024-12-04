2eccde2e0575d3fdeea9331bb101bcb2c6e53856
BJS-kr <hahajeng1234@gmail.com>
2024-12-05 00:03:54 +0900

    fix: pick character types befor users move to game handler

--- controller/handler_room.go
+++ controller/handler_room.go
@@ -31,6 +31,7 @@ func (c *Controller) roomHandler(input *message.Input) (*message.Input, error) {
 		roomInput.RoomState = r.MakeRoomState(roomInput.RoomName)
 	case message.Room_START:
 		r.StopStreaming()
+		characterTypes := r.PickCharacterRandomTypes()
 
 		for _, userId := range r.Users.LockedIds() {
 			r.Game.State.AddUserState(state.UserID(userId))
@@ -55,7 +56,7 @@ func (c *Controller) roomHandler(input *message.Input) (*message.Input, error) {
 
 		r.Game.StartStreaming(c.makeGameHandler(r))
 		r.SetStatus(message.RoomState_PLAYING)
-		r.Game.SystemDirectInput(c.MakeGameStartInput(input.UserId, roomInput.RoomName, r.PickCharacterRandomTypes()))
+		r.Game.SystemDirectInput(c.MakeGameStartInput(input.UserId, roomInput.RoomName, characterTypes))
 	case message.Room_QUIT:
 		u, err := c.Rooms.Quit(roomInput.RoomName, input.UserId)
 
